de5e8abc342535eba5d1b59ca80638a205d43f52
Timofey <epanchee@gmail.com>
2015-05-25 20:19:48 +0500

    add MatImageRecordWriter based on byteArray

--- src/main/java/core/recordReaders/MatImage/MatImageRecordWriter.java
+++ src/main/java/core/recordReaders/MatImage/MatImageRecordWriter.java
@@ -4,10 +4,8 @@ import core.recordReaders.ImageRecordWriter;
 import core.writables.MatImageWritable;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.opencv.core.Mat;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import org.opencv.core.MatOfByte;
+import org.opencv.highgui.Highgui;
 
 /**
  * Created by Epanchee on 24.02.15.
@@ -19,21 +17,8 @@ public class MatImageRecordWriter extends ImageRecordWriter<MatImageWritable> {
 
     @Override
     protected void writeImage(MatImageWritable image, FSDataOutputStream imageFile) throws Exception {
-        BufferedImage out;
-        Mat in = image.getImage();
-        byte[] data = new byte[in.width() * in.height() * (int)in.elemSize()];
-        int type;
-        in.get(0, 0, data);
-
-        if(in.channels() == 1)
-            type = BufferedImage.TYPE_BYTE_GRAY;
-        else
-            type = BufferedImage.TYPE_3BYTE_BGR;
-
-        out = new BufferedImage(in.width(), in.height(), type);
-
-        out.getRaster().setDataElements(0, 0, in.width(), in.height(), data);
-
-        ImageIO.write(out, image.getFormat(), imageFile);
+        MatOfByte mob = new MatOfByte();
+        Highgui.imencode("."+image.getFormat(), image.getImage(), mob);
+        imageFile.write(mob.toArray());
     }
 }
