0b9758941636cee0adb92b127f21ef8005740777
Timofey <epanchee@gmail.com>
2015-05-26 11:35:26 +0500

    exclude mipr strings

--- src/main/java/core/MiprMain.java
+++ src/main/java/core/MiprMain.java
@@ -9,11 +9,10 @@ import java.net.URI;
  */
 public class MiprMain {
 
-    public static final String workspace = "/user/u1220/";
+    public static final String workspace = "";
 
     public static URI getOpenCVUri(){
-//        return new Path(workspace + "/usr/local/share/OpenCV/java/libopencv_java2411.so").toUri();
-        return new Path(workspace + "/lib/libopencv_java2411.so").toUri();
+        return new Path(workspace + "/usr/local/share/OpenCV/java/libopencv_java2411.so").toUri();
     }
 
 }
