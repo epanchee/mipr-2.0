6e4c14a8a178a221356c82e1d4c164cd1feecd23
Timofey <epanchee@gmail.com>
2015-05-26 19:23:38 +0500

    add SequenceFile packager for MatImage

---
+++ src/main/java/core/utils/SequenceFileMatImagePackager.java
@@ -0,0 +1,53 @@
+package core.utils;
+
+import core.MiprMain;
+import core.formats.MatImage.CombineMatImageInputFormat;
+import core.writables.MatImageWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by Epanchee on 26.05.2015.
+ */
+public class SequenceFileMatImagePackager {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        String input = args[0];
+        String output = args[1];
+
+        Configuration conf = new Configuration();
+        DistributedCache.addCacheFile(MiprMain.getOpenCVUri(), conf);
+        Job job = new Job(conf);
+        job.setJarByClass(SequenceFileMatImagePackager.class);
+        job.setMapperClass(SequenceFileMatImagePackagerMapper.class);
+        job.setNumReduceTasks(3); // count of result seq files
+        job.setInputFormatClass(CombineMatImageInputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class); // that is all
+        Path outputPath = new Path(output);
+        FileInputFormat.setInputPaths(job, input);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(MatImageWritable.class);
+        outputPath.getFileSystem(conf).delete(outputPath, true); // delete folder if exists
+
+        job.waitForCompletion(true);
+
+    }
+
+    static class SequenceFileMatImagePackagerMapper extends Mapper<NullWritable, MatImageWritable, NullWritable, MatImageWritable>{
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            Path[] myCacheFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+            System.load(myCacheFiles[0].toUri().getPath());
+        }
+    }
+}
