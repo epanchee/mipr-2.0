a3798b8b2b3804c45d935afaee129fd78dc16e8e
Timofey <epanchee@gmail.com>
2015-05-27 14:06:47 +0500

    allocate more memory for map & reduce tasks

--- src/main/java/core/utils/SequenceFileMatImagePackager.java
+++ src/main/java/core/utils/SequenceFileMatImagePackager.java
@@ -26,8 +26,8 @@ public class SequenceFileMatImagePackager {
 
         Configuration conf = new Configuration();
         DistributedCache.addCacheFile(MiprMain.getOpenCVUri(), conf);
-        conf.set("mapreduce.map.memory.mb", "4096");
-        conf.set("mapreduce.reduce.memory.mb", "4096");
+        conf.set("mapreduce.map.memory.mb", "3000");
+        conf.set("mapreduce.reduce.memory.mb", "3000");
         Job job = new Job(conf);
         job.setJarByClass(SequenceFileMatImagePackager.class);
         job.setMapperClass(SequenceFileMatImagePackagerMapper.class);
