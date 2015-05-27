abc37e83cf46e466926e6dce99e79f1624909886
Timofey <epanchee@gmail.com>
2015-05-27 15:06:49 +0500

    add image type to serialization. change heap size to 1250

--- src/main/java/experiments/Img2Gray_opencv_combine.java
+++ src/main/java/experiments/Img2Gray_opencv_combine.java
@@ -28,7 +28,7 @@ public class Img2Gray_opencv_combine {
 
         Configuration conf = new Configuration();
         DistributedCache.addCacheFile(MiprMain.getOpenCVUri(), conf);
-        conf.set("mapreduce.map.memory.mb", "3000");
+        conf.set("mapreduce.map.memory.mb", "1250");
         Job job = new Job(conf);
         job.setJarByClass(Img2Gray_opencv_combine.class);
         job.setMapperClass(Img2Gray_opencvMapper.class);
